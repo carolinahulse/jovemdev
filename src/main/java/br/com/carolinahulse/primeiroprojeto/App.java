21a7a3021c71457a471a896a15c2937c4143be67
carolinahulse <carolinahulse321@gmail.com>
2022-05-30 16:39:14 -0300

    Primeiro commit

---
+++ src/main/java/br/com/carolinahulse/primeiroprojeto/App.java
@@ -0,0 +1,10 @@
+package br.com.carolinahulse.primeiroprojeto;
+
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Olá");
+    }
+}
